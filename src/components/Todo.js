8ee3c6542cfa63fc47d009e354e80bca7d832ff0
Sean Birmingham <seannbirmingham98@gmail.com>
2020-02-04 21:36:14 -0500

    styled todo list

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Todo = ({ todo, toggleTodo }) => {
     return (
-        <div className={`${todo.completed ? " completed" : ""}`} onClick={() => toggleTodo(todo.id)}>
+        <div className={`todo${todo.completed ? " completed" : ""}`} onClick={() => toggleTodo(todo.id)}>
             <p>{todo.item}</p>
         </div>
     )
